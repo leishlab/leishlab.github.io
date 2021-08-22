cbb242aa659dde27e63d64fc3bddefe15e07351a
moreamora1 <felipe@moreamora.com.br>
2021-08-22 19:08:40 -0300

    ADD basic routes

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="relative overflow-hidden">
+        <cheader />
+        <Nuxt />
+        <cfooter />
+    </div>
+</template>
+
+<script>
+import header from "@/components/header.vue";
+import footer from "@/components/footer.vue";
+
+export default {
+  components: {
+    cheader: header,
+    cfooter: footer,
+  },
+};
+</script>
---
+++ pages/publications/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Publications</h1>
+    </div>
+</template>
