4ba72f94a57906f92b0b4f5986a434f5813a7850
leishlab <89360010+leishlab@users.noreply.github.com>
2021-10-12 22:14:27 -0300

    links lattes linkedin

--- pages/people/index.vue
+++ pages/people/index.vue
@@ -18,7 +18,7 @@ export default {
                 {
                     id: 1,
                     name: 'Maria Fernanda Laranjeira-Silva',
-                    role: 'Research Associate / USP',
+                    role: 'Research Associate - USP',
                     lattesUrl: 'https://lattes.cnpq.br/5423200994091318',
                     linkedinUrl: 'https://www.linkedin.com/in/maria-fernanda-laranjeira-da-silva-91a14526/',
                     imageUrl:
@@ -28,8 +28,8 @@ export default {
                 {
                     id: 2,
                     name: 'Ahyun Avery Hong',
-                    role: 'PhD Student / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'PhD Student - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/6341417420423481',
                     linkedinUrl: 'https://www.linkedin.com/in/ahyun-avery-hong/',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1634046365/Avery_feLAB_yc7iwc.jpg',
@@ -38,8 +38,8 @@ export default {
                 {
                     id: 3,
                     name: 'Romario Lopes Boy',
-                    role: 'MS Student / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'MS Student - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/6780260214719643',
                     linkedinUrl: 'https://www.linkedin.com/in/rom%C3%A1rio-lopes-boy-44113674/',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1633544219/Romario_hfrf9m.jpg',
@@ -48,8 +48,8 @@ export default {
                 {
                     id: 4,
                     name: 'Lucas Felipe Almeida Athayde',
-                    role: 'MS Student / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'MS Student - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/1590201488081535',
                     linkedinUrl: 'https://linkedin.com',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1634070695/Lucas_FeLAB_mjjmef.jpg',
@@ -58,8 +58,8 @@ export default {
                 {
                     id: 5,
                     name: 'Giovana Aquino Parreira',
-                    role: 'Undergrad Student / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'Undergrad Student - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/0418647232202748',
                     linkedinUrl: 'https://linkedin.com',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1633544270/Giovana_zju6pm.jpg',
@@ -68,8 +68,8 @@ export default {
                 {
                     id: 6,
                     name: 'Marco Antonio Mendes Gomes',
-                    role: 'Undergrad Student / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'Undergrad Student - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/2681040462942756',
                     linkedinUrl: 'https://linkedin.com',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1633540924/Marco_nk6zbe.jpg',
@@ -78,8 +78,8 @@ export default {
                 {
                     id: 7,
                     name: 'Lucile Maria Floeter-Winter',
-                    role: 'Professor / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'Professor - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/9468443035508043',
                     linkedinUrl: 'https://www.linkedin.com/in/lucile-floeter-winter-b5b91a48/',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1634045145/Lucile_FeLAB_foto_xvtn8b.jpg',
@@ -88,8 +88,8 @@ export default {
                 {
                     id: 8,
                     name: 'Juliana Ide Aoki',
-                    role: 'Post-Doc / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'Post-Doc - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/3300018120206040',
                     linkedinUrl: 'https://www.linkedin.com/in/juliana-aoki-880669153/',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1633481980/foto_JuAoki_site_wqvx5j.png',
@@ -98,8 +98,8 @@ export default {
                 {
                     id: 9,
                     name: 'Ricardo Andrade Zampieri',
-                    role: 'Researcher / USP',
-                    twitterUrl: 'https://twitter.com',
+                    role: 'Researcher - USP',
+                    lattesUrl: 'http://lattes.cnpq.br/6456862196184646',
                     linkedinUrl: 'https://www.linkedin.com/in/ricardo-andrade-zampieri-6440274a/',
                     imageUrl:
                     'https://res.cloudinary.com/dppi8hoxw/image/upload/v1633540924/Ricardo_lodike.jpg',
