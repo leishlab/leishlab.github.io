6d31ebacdd455cdb07b09d6d6681ea35c3691aa7
leishlab <leishlab@users.noreply.github.com>
2021-10-13 02:40:47 +0000

    deploy: c93336af7bcfc6cd84119d6675d33a2ed8fa8401

---
+++ _nuxt/static/1634092846/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fbio","\u002Fcontact","\u002Fpeople","\u002Fprivacy","\u002Fterms","\u002Fcollaborations","\u002Fpublications","\u002Fresearch"]})
