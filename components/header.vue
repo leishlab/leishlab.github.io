249a76db1440003d66714915ef9573c9171eb9a2
moreamora1 <felipe@moreamora.com.br>
2021-10-17 22:59:34 -0300

    BEGIN fix menu celular

--- components/header.vue
+++ components/header.vue
@@ -1,87 +1,54 @@
 <template>
-    <header class="relative">
-      <div class="bg-gray-900">
-        <nav class="relative max-w-7xl mx-auto flex items-center justify-between px-4 py-4 sm:px-6" aria-label="Global">
-          <div class="flex items-center flex-1">
-            <div class="flex items-center justify-between w-full md:w-auto">
-              <a href="/">
-                <span class="sr-only">Workflow</span>
-                <Logo v-bind:color="logo_color"/>
-                <!--img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-teal-200-cyan-400.svg" alt=""-->
-              </a>
-              <div class="-mr-2 flex items-center md:hidden">
-                <button type="button" class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white" aria-expanded="false">
-                  <span class="sr-only">Open main menu</span>
-                  <!-- Heroicon name: outline/menu -->
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-            <ul class="hidden space-x-8 md:flex md:ml-10">
-              <li 
-                v-for="item in menus"
-                :key="item.id"
-              >
-                <NuxtLink
-                  class="text-base font-medium text-white hover:text-gray-300"
-                  :to="'/' + item.page"
-                >{{ item.text }}</NuxtLink>
-              </li>
-            </ul>
-          </div>
-          <!--div class="hidden md:flex md:items-center md:space-x-6">
-            <a href="#" class="text-base font-medium text-white hover:text-gray-300">
-              Log in
-            </a>
-            <a href="#" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700">
-              Start free trial
-            </a>
-          </div-->
-        </nav>
-      </div>
+  <header
+    :class="headerClassList"
+    class="sticky h-16 top-0 w-full bg-gray-900 z-50 flex justify-center"
+  >
+    <div
+      class="max-w-screen-lg flex-1 flex w-full justify-between px-6 items-center lg:p-0"
+    >
+      <!--logo-->
+      <a href="/">
+        <Logo v-bind:color="logo_color"/>
+      </a>
 
-      <!--
-        Mobile menu, show/hide based on menu open state.
-
-        Entering: "duration-150 ease-out"
-          From: "opacity-0 scale-95"
-          To: "opacity-100 scale-100"
-        Leaving: "duration-100 ease-in"
-          From: "opacity-100 scale-100"
-          To: "opacity-0 scale-95"
-      -->
-      <div class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-        <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-          <div class="px-5 pt-4 flex items-center justify-between">
-            <div>
-              <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg" alt="">
-            </div>
-            <div class="-mr-2">
-              <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
-                <span class="sr-only">Close menu</span>
-                <!-- Heroicon name: outline/x -->
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div class="pt-5 pb-6">
-            <div class="px-2 space-y-1">
-              <NuxtLink
-                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                v-for="item in menus"
-                :key="item.id"
-                :to="'/' + item.page"
-                >{{ item.text }}
-                </NuxtLink>
-            </div>
-          </div>
-        </div>
-      </div>
-    </header>
+      <!--navigation-->
+      <nav
+        class="w-full flex flex-auto flex-col items-end lg:w-1/2 lg:flex-row lg:items-center lg:h-full border-white"
+      >
+        <!--hidden hamburger button-->
+        <button
+          class="flex items-center text-white mt-1 lg:hidden"
+          @click.prevent.stop="onToggleClick"
+          aria-label="Menu"
+        >
+          <svg
+            class="fill-current h-6 w-6"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+        <ul
+          :class="navContentClassList"
+          class="px-6 absolute bg-gray-900 w-full top-16 left-0 right-0 list-reset lg:flex lg:static lg:justify-end lg:px-0 lg:h-full"
+        >
+          <li
+            class="tracking-wider w-full lg:w-auto lg:ml-5 lg:h-full lg:flex lg:items-center"
+            v-for="item in menus"
+            :key="item.id"
+          >
+            <NuxtLink
+              class="text-base text-right font-medium text-white hover:border-white hover:text-gray-200"
+              :to="'/' + item.page"
+              >{{ item.text }}</NuxtLink
+            >
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -126,5 +93,46 @@ export default {
     };
   },
 
+  computed: {
+    isSticky() {
+      return this.scrollY > 10;
+    },
+    headerClassList() {
+      return this.isSticky ? "shadow" : "";
+    },
+    navActionClassList() {
+      return this.isSticky ? "text-white" : "text-gray-900";
+    },
+    navContentClassList() {
+      let classList = this.isSticky ? "bg-gray-100" : "bg-gray-100";
+
+      if (!this.isOpen) {
+        classList += ` hidden`;
+      }
+
+      return classList;
+    }
+  },
+
+  methods: {
+    onClick() {
+      this.isOpen = false;
+    },
+    onScroll() {
+      this.scrollY = window.scrollY;
+    },
+    onToggleClick() {
+      this.isOpen = !this.isOpen;
+    }
+  },
+  mounted() {
+    this.scrollY = window.scrollY;
+    document.addEventListener("click", this.onClick);
+    document.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    document.removeEventListener("click", this.onClick, true);
+    document.removeEventListener("scroll", this.onScroll, true);
+  }
 }
 </script>
