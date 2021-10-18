7545a846d6bf161f2062ef0abb21ffa876c71a2a
leishlab <89360010+leishlab@users.noreply.github.com>
2021-10-18 15:12:25 -0300

    bio

--- pages/bio/index.vue
+++ pages/bio/index.vue
@@ -5,7 +5,7 @@
         <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
         <div>
             <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Case Study</h2>
-            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Meet Whitney</h3>
+            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Meet Maria Fernanda</h3>
         </div>
         </div>
         <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
@@ -35,14 +35,16 @@
         </div>
         <div class="mt-8 lg:mt-0">
             <div class="text-base max-w-prose mx-auto lg:max-w-none">
-            <p class="text-lg text-gray-500">Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi.</p>
+            <p class="text-lg text-gray-500">My long-term research interests are to identify the genes and pathways responsible for the transport of heme and transition metals like iron, manganese and zinc in Leishmania, focusing on the competition for these essential nutrients at the Leishmania-host interface. My ultimate research goal is to define therapeutic targets for leishmaniasis treatment.</p>
             </div>
             <div class="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-            <p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.</p>
-            <p>Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.</p>
-            <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
+            <p>I received my undergraduate degree in Molecular Sciences and a Ph.D. from the University of Sao Paulo, Brazil. During my PhD, Dr. Lucile Maria Floeter-Winter was my advisor, and I also worked in an interniship at Steve Beverley’s laboratory at Washington University of St Louis (USA).</p>
+            <p>As a graduate student, I elucidated the role of Leishmania arginase that defines the fate of L-arginine and showed that correct subcellular localization of arginase is essential for enzyme activity and optimal parasite infectivity.</p>
+            <p>As a postdoctoral fellow at USP, Brazil, I discovered that the hormone melatonin attenuates Leishmania burden and lesion progression in hamsters.</p>
+            <p>My interests in iron and heme metabolism were sparked when I joined Dr. Norma Andrews’ lab at the University of Maryland as a Post-Doctoral Fellow, and then Dr. Iqbal Hamza’s lab as a Research Associate. In the Andrews’ lab, I identified and characterized the first iron exporter (LIR1) in a trypanosomatid parasite and showed that iron export is essential for Leishmania iron homeostasis and infectivity thereby answering an important question about how these parasites prevent accumulation of free iron to avoid cytotoxicity. In the Hamza lab, I gained a deeper understanding for host iron and heme metabolism pathways by studying the function of mouse and human HRG1.<p>
+            <p>I started my independent career as an early-stage investigator in Brazil when I was competitively awarded a Young Investigator FAPESP grant.<p> 
             <ul role="list">
-                <li>Quis elit egestas venenatis mattis dignissim.</li>
+                <li>B.S. in Molecular Sciences (USP/Brazil) - 2001 – 2005.</li>
                 <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
                 <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
             </ul>
