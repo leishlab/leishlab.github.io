e0d43e28536302193696ea484c151c18d07f0631
leishlab <89360010+leishlab@users.noreply.github.com>
2023-07-26 17:06:37 -0300

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -24,7 +24,7 @@ export default {
           contact_address_street: "Rua do Matão, travessa 14, 101",
           contact_address_place: 'Cidade Universitária - São Paulo - SP',
           contact_address_zip: '05508-090',
-          contact_phone: '+55 (11) 2648-8430',
+          contact_phone: '+55 (11) 3091-7503',
           contact_email: 'mfernandals@usp.br',
           page_illustration: "https://res.cloudinary.com/dppi8hoxw/image/upload/v1634655327/undraw_alien_science_nonm_m4fwoe.svg"
         }
